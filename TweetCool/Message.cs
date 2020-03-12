1a7c0b1f98eaa2b4a622c64cbfd5f2033a178ae5
makakoocelot <makakoocelot@gmail.com>
2020-03-12 10:43:59 +0100

    Add project files.

---
+++ TweetCool/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetCool
+{
+    public class Message
+    {
+        public DateTime PostTime { get; set; }
+        public string Text { get; set; }
+        public string Username { get; set; }
+
+        public Message(string username, string text)
+        {
+            Username = username;
+            Text = text;
+            PostTime = DateTime.Now;
+        }
+    }
+}
---
+++ TweetCool/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace TweetCool.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        
+
+        
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost([FromForm(Name = "username")] string username , [FromForm(Name = "message")] string message)
+        {
+            List<Message> messages;
+        
+            if (AllMessageModel.Messages.ContainsKey(username))
+            {
+                messages = AllMessageModel.Messages[username];
+            }
+           
+            else
+            {
+                messages = new List<Message>();
+                AllMessageModel.Messages[username] = messages;
+            }
+            messages.Add(new Message(username, message));
+            Response.Redirect("/AllMessage");
+        }
+    }
+}
